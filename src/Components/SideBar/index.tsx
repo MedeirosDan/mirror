77b2f74f001ae14db5d1a1ace6df9687412168ca
MedeirosDan <web.medeiros@gmail.com>
2024-07-05 16:02:25 -0300

    ajuste sidebar

--- src/Components/SideBar/index.tsx
+++ src/Components/SideBar/index.tsx
@@ -19,32 +19,33 @@ export default function SideBar() {
         { page: "Teste 2", link: "teste2" },
         { page: "Teste 3", link: "teste3" },
         { page: "Teste 4", link: "teste4" },
-        { page: "Teste 5", link: "teste5" }
-      ]
-    }
+        { page: "Teste 5", link: "teste5" },
+      ],
+    },
   ];
 
   return (
     <div className={styles.sideBar}>
       {projects.map((project, projectIndex) => (
-        <div key={projectIndex}>
-          <div className={styles.sideBarTitle}>
+        <div key={projectIndex} className={styles.sideBarMenu}>
+          <div className={styles.sideBarMenuTitle}>
             <h4>{project.name}</h4>
           </div>
           <div className={styles.sideBarLinksProject}>
             <ul>
               {project.pages.map((page, pageIndex) => (
                 <li key={pageIndex}>
-                  <a href={page.link}>{page.page}</a><button>Editar</button>
+                  <a href={page.link}>{page.page}</a>
+                  <button>Editar</button>
                 </li>
               ))}
             </ul>
           </div>
-          <div className={styles.sideBarLogouf}>
-            <a href="#">Logout</a>
-          </div>
         </div>
       ))}
+      <div className={styles.sideBarLogouf}>
+        <a href="#">Logout</a>
+      </div>
     </div>
   );
 }
